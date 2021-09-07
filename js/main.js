856dc07c8f7e114b0a9f1a6d469d320179c29a3f
Ruslan0111 <gadilovruslan@yandex.ru>
2021-09-07 20:42:48 +0500

    header provided

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  document.querySelector('.header__burger').addEventListener('click', () => {
+    document.querySelector('.header__burger').classList.toggle('active');
+    document.querySelector('.header__menu').classList.toggle('active');
+    document.querySelector('body').classList.toggle('lock');
+  })
+}
