96b3f67bf96480301c71f39616cc27d97b19a116
Ruslan0111 <gadilovruslan@yandex.ru>
2021-09-08 14:35:48 +0500

    slider and news section provided

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ window.onload = () => {
     document.querySelector('.header__menu').classList.toggle('active');
     document.querySelector('body').classList.toggle('lock');
   })
+
+  let swiper = new Swiper(".swiper", {
+    autoplay: {
+      disableOnInteraction: false
+    }
+  });
 }
